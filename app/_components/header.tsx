738de37099bab2d59aeb9a76a2d9335ee4aad6f4
Nilson <netohjunior216@gmail.com>
2025-11-10 19:28:27 -0300

    feat: add menu component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,14 +1,42 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import { MenuIcon, MessageCircleIcon } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import SidebarMenu from "./sidebar-menu";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="bg-white flex items-center justify-between px-5 py-6">
+    <header className="flex items-center justify-between bg-white px-5 py-6">
       <Image src="/logo.svg" alt="Aparatus" width={100} height={26.09} />
-      <Button variant="outline" size="icon">
-        <MenuIcon />
-      </Button>
+      <div className="flex items-center gap-2">
+        <Button variant="outline" size="icon" asChild>
+          <Link href="/chat">
+            <MessageCircleIcon />
+          </Link>
+        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-[370px] p-0">
+            <SheetHeader className="border-b px-5 py-6 text-left">
+              <SheetTitle className="text-lg font-bold">Menu</SheetTitle>
+            </SheetHeader>
+            <SidebarMenu />
+          </SheetContent>
+        </Sheet>
+      </div>
     </header>
   );
 };
